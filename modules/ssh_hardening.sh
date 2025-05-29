3afb672b80624b4ea530757ad5173e0a7dd0ac8a
AO <alperoztvrk@hotmail.com>
2025-05-29 11:06:52 +0100

    Update ssh_hardening.sh

--- modules/ssh_hardening.sh
+++ modules/ssh_hardening.sh
@@ -2,16 +2,10 @@
 
 echo "SSH Hardening started..."
 
-# SSH config dosyasının yedeğini al
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak.$(date +%Y%m%d_%H%M%S)
 
-# Root login'i devre dışı bırak
 sed -i 's/^#*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 
-# Şifreli girişleri devre dışı bırakmak istersen:
-# sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
-
-# SSH servisini yeniden başlat
 systemctl restart ssh
 
 echo "SSH Hardening completed."
