a82c5f57c202220efea9a44348b2111ea03c1b23
AO <alperoztvrk@hotmail.com>
2025-05-29 11:06:30 +0100

    Update service_hardening.sh

--- modules/service_hardening.sh
+++ modules/service_hardening.sh
@@ -2,11 +2,9 @@
 
 echo "Service Hardening started..."
 
-# 1. Aktif çalışan servisleri listele
 echo -e "\nActive running services:"
 systemctl list-units --type=service --state=running
 
-# 2. Güvenli olmayan bilinen servisleri kontrol et (örnek: telnet, rsh, vsftpd, nfs, avahi, cups, rpcbind)
 POTENTIAL_RISKS=(telnet rsh vsftpd nfs avahi-daemon cups rpcbind)
 
 echo -e "\nPotantial dangerous services:"
